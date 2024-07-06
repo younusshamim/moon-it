84d008bd154e8c9467cb9aff104a3b4bdb9214a5
Younus Shamim <younusshamim.bd@gmail.com>
2024-07-06 20:12:13 +0600

    set dark mode and light mode separate color for next progress

--- src/components/next-progress.tsx
+++ src/components/next-progress.tsx
@@ -1,7 +1,17 @@
 'use client';
 
+import { useTheme } from 'next-themes';
 import NextTopLoader from 'nextjs-toploader';
 
 export default function NextProgress() {
-  return <NextTopLoader color="#14b9fc" showSpinner={false} crawlSpeed={100} speed={100} />;
+  const { theme, setTheme } = useTheme();
+
+  return (
+    <NextTopLoader
+      color={theme === 'dark' ? "#00a5e8" : '#f89a4d'}
+      showSpinner={false}
+      crawlSpeed={100}
+      speed={100}
+    />
+  );
 }
